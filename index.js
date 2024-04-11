773927a3bf09ff3a57d8e11a57efa25b9d995c33
Sajid <sajidmohammad460519@gmail.com>
2024-04-11 12:26:07 +0800

    setting grid size and drawing works, alhamdulilah

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
-const gridDiv = document.querySelector(".grid-div");
-let rows = [];
-
-for (let j = 0; j < 16; j++) {
-  rows[j] = document.createElement("div");
-  rows[j].classList.add("row-style");
-  gridDiv.appendChild(rows[j]);
-  for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    div.style.cssText = "background-color: red; height: 50px;";
-    // div.classList.add("")
-    rows[j].appendChild(div);
-  }
-}
-
-const gridRows = document.querySelectorAll(".row-style div");
-
-gridRows.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.style.cssText = "background-color: white; height: 50px; width: 50px";
-  });
-});
+initGrid();
+mouseOverListFunction();
 
 const btn = document.querySelector(".square-settings");
 
 btn.addEventListener("click", () => {
   const userInput = prompt("choose how many squares you want");
-  let divSize = 200 / parseInt(userInput) 
+  let divSize = parseInt(userInput);
   const body = document.getElementsByTagName("body")[0];
-  const dynamicGridDiv = document.querySelector(".grid-div")
-  body.removeChild(dynamicGridDiv);
+  const gridDiv = document.querySelector(".grid-div");
+  body.removeChild(gridDiv);
   let newGridDiv = document.createElement("div");
   newGridDiv.classList.add("grid-div");
-  body.appendChild(newGridDiv)
-  for (let j = 0; j < userInput; j++) {
+  body.appendChild(newGridDiv);
+  let rows = [];
+  for (let j = 0; j < divSize; j++) {
     rows[j] = document.createElement("div");
     rows[j].classList.add("row-style");
     newGridDiv.appendChild(rows[j]);
-    for (let i = 0; i < userInput; i++) {
+    for (let i = 0; i < divSize; i++) {
       const div = document.createElement("div");
-      div.style.cssText = `background-color: red; height: ${divSize}px; width: ${100}px`;
+      div.classList.add("div-init");
       rows[j].appendChild(div);
     }
   }
+  mouseOverListFunction()
 });
+
+function initGrid() {
+  const gridDiv = document.querySelector(".grid-div");
+  let rows = [];
+
+  for (let j = 0; j < 16; j++) {
+    rows[j] = document.createElement("div");
+    rows[j].classList.add("row-style");
+    gridDiv.appendChild(rows[j]);
+    for (let i = 0; i < 16; i++) {
+      const div = document.createElement("div");
+      div.classList.add("div-init");
+      rows[j].appendChild(div);
+    }
+  }
+}
+
+function mouseOverListFunction() {
+  const gridRows = document.querySelectorAll(".row-style div");
+
+  gridRows.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.style.cssText = "background-color: white";
+    });
+  });
+}
