010fa74ab224803f2a704e3a0d7a29453bb23a73
Sajid <sajidmohammad460519@gmail.com>
2024-04-11 08:43:27 +0800

    red to white when hovered, 16x16 divs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-etch-a-sketch
+
+view website here: https://sajidmohd717.github.io/odin-etch-a-sketch/
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const gridDiv = document.querySelector(".grid-div");
-let rows = []
+let rows = [];
 
 for (let j = 0; j < 16; j++) {
-    rows[j] = document.createElement("div");
-    rows[j].style.cssText = "display: flex; flex-direction: column; gap: 10px"
-    gridDiv.appendChild(rows[j])
+  rows[j] = document.createElement("div");
+  rows[j].classList.add("row-style");
+  gridDiv.appendChild(rows[j]);
   for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
     div.style.cssText = "background-color: red; height: 50px; width: 50px";
     rows[j].appendChild(div);
   }
 }
+
+const gridRows = document.querySelectorAll(".row-style div");
+
+gridRows.forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    div.style.cssText = "background-color: white; height: 50px; width: 50px";
+  });
+});
